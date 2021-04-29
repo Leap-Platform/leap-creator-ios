f16ed4d683992babd6ee16684629ad26d4676a95
aravind_jiny <aravind@jiny.io>
2021-04-29 23:44:23 +0530

    Downloading dependencies in swift package

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
-        .binaryTarget(name: "LeapCreatorSDK", path: "LeapCreatorSDK.xcframework")
+        .binaryTarget(
+            name: "LeapCreatorSDK", 
+            path: "LeapCreatorSDK.xcframework",
+            dependencies:["Starscream"]
+        )
     ]
 )

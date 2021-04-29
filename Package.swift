a332a3c6ae9faa0f271549adb35b3a6cf6bff98c
aravind_jiny <aravind@jiny.io>
2021-04-29 23:24:13 +0530

    LeapCreatorSDK v1.0.0-alpha1 release

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "LeapCreatorSDK",
+    products: [
+        .library(name: "LeapCreatorSDK", targets: ["LeapCreatorSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+        .binaryTarget(name: "LeapCreatorSDK", path: "LeapCreatorSDK.xcframework")
+    ]
+)

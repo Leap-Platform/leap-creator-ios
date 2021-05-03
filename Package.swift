694b7dc2fbafb73cbae3e22a772695f03ef2baac
aravind_jiny <aravind@jiny.io>
2021-05-03 17:28:28 +0530

    v0.0.2 release

--- Package.swift
+++ Package.swift
@@ -5,15 +5,20 @@ import PackageDescription
 let package = Package(
     name: "LeapCreatorSDK",
     products: [
-        .library(name: "LeapCreatorSDK", targets: ["LeapCreatorSDK"]),
+        .library( name: "LeapCreatorSDK", targets: ["LeapCreatorSDKTargets"] )
     ],
     dependencies: [
-        .package(name:"Starscream", url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.0"))
+        .package(name: "Starscream", url: "https://github.com/daltoniam/Starscream.git",.upToNextMajor(from: "4.0.0"))
     ],
     targets: [
         .binaryTarget(
-            name: "LeapCreatorSDK", 
-            path: "LeapCreatorSDK.xcframework"
-        )
+            name: "LeapCreatorSDK", path: "LeapCreatorSDK.xcframework"
+        ),
+        .target(name: "LeapCreatorSDKTargets",
+                dependencies: [
+                    .target(name: "LeapCreatorSDK"),
+                    .product(name: "Starscream", package: "Starscream")
+                ],
+                path: "Sources")
     ]
 )
